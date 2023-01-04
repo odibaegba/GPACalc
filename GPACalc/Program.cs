5ccbe2f3282263008690199e5c570a540383482a
odibaegba <112698852+odibaegba@users.noreply.github.com>
2023-01-04 09:38:43 -0800

    New Commit

---
+++ GPACalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using GPACalc.UI;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UserInput input = new UserInput();
+        input.TakeInputs();
+        Console.ReadLine();
+    }
+}
